a5aef71a77d71ac1c38346653fe121d81b073442
AKJANEWA <ashishjanewa1@gmail.com>
2018-12-01 14:16:15 +0000

    This is 1st cheft push

---
+++ apache/recipes/apache_configure_httpd.rb
@@ -0,0 +1,11 @@
+#
+### Cookbook:: httpd
+### Recipe:: default
+###
+### Copyright:: 2018, The Authors, All Rights Reserved.
+#
+#cookbook_file "/var/www/html/index.html" do
+#  source "index.html.erb"
+#    mode "0677"
+#    end
+#
---
+++ apache/recipes/apache_deploy.rb
@@ -0,0 +1,10 @@
+#
+### Cookbook:: httpd
+### Recipe:: default
+###
+### Copyright:: 2018, The Authors, All Rights Reserved.
+#
+#service 'httpd' do
+#  action [ :enable, :start ]
+#  end
+#
